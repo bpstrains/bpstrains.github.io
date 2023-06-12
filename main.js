88f16fcdc13a10001eeb2038e539dd5ce3c399e3
bpstrains <128532085+bpstrains@users.noreply.github.com>
2023-06-12 16:21:03 +0000

    Updates

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 function isLoggedIn() {
+    var hash = "b17b49f2a7b26a487e88c2a483ab9443";
     var cookies = document.cookie.split(";");
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i].trim();
       if (cookie.indexOf("loggedIn=") == 0) {
-        return cookie.substring("loggedIn=".length, cookie.length) == "true";
+        return cookie.substring("loggedIn=".length, cookie.length) == hash;
       }
     }
     return false;
